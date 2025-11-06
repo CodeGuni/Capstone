181253a8d98ecd0665ee470c33cc3bd3e3e6c292
Mandeep <engg.mandeep2002@gmail.com>
2025-11-06 18:07:59 -0500

    Fix: cleaned search.module.ts merge conflict

--- backend-ts/src/search/search.module.ts
+++ backend-ts/src/search/search.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { SearchController } from './search.controller';
 import { SearchService } from './search.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
-  controllers: [SearchController],
-  providers: [SearchService],
-<<<<<<< HEAD
-    exports: [SearchService],
-=======
->>>>>>> 91a16942160d47dfadf241795dde0cff6593b312
+  providers: [SearchService, PrismaService],
+  exports: [SearchService],
 })
 export class SearchModule {}
